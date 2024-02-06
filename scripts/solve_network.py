62ed7fa37e0505afc5326feb33dde797bea16a2a
Koen van Greevenbroek <koen.v.greevenbroek@uit.no>
2024-02-06 13:40:13 +0000

    Add option to disable print infeasibilities

--- scripts/solve_network.py
+++ scripts/solve_network.py
@@ -1245,9 +1245,10 @@ def solve_network(n, config, solving, **kwargs):
             f"Solving status '{status}' with termination condition '{condition}'"
         )
     if "infeasible" in condition:
-        labels = n.model.compute_infeasibilities()
-        logger.info(f"Labels:\n{labels}")
-        n.model.print_infeasibilities()
+        if cf_solving.get("print_infeasibilities", True):
+            labels = n.model.compute_infeasibilities()
+            logger.info(f"Labels:\n{labels}")
+            n.model.print_infeasibilities()
         raise RuntimeError("Solving status 'infeasible'")
 
     return n
