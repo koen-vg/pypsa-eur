2dd52b1ffd9d22a093f8e8ae9576f3be39164737
Koen van Greevenbroek <koen.v.greevenbroek@uit.no>
2024-03-14 12:04:48 +0100

    Add skeleton of scenario creation
    Instead of a set product of values for individual config keys, this
    scenario creation template enables the set product of named groups of
    configuration settings.

--- config/create_scenarios.py
+++ config/create_scenarios.py
@@ -16,22 +16,82 @@ import itertools
 # Insert your config values that should be altered in the template.
 # Change `config_section` and `config_section2` to the actual config sections.
 template = """
-scenario{scenario_number}:
-    config_section:
-        config_key: {config_value}
-    config_section2:
-        config_key2: {config_value2}
+run{scenario_number}:
+    snapshots:
+        start: {snapshot_start}
+        end: {snapshot_end}
+    sector:
+        co2_sequestration_potential: {co2_sequestration_potential}
+    adjustments:
+        sector:
+            capital_cost:
+                "H2 Electrolysis": {electrolysis_cost}
+                "SMR CC": {smr_cc_cost}
+                "SMR": {smr_cost}
+                "nuclear": {nuclear_cost}
+            marginal_cost:
+                "gas": {gas_marginal_cost}
 """
 
-# Define all possible combinations of config values.
-# This must define all config values that are used in the template.
-config_values = dict(config_value=["true", "false"], config_value2=[1, 2, 3, 4])
+scenarios = {
+    "weather_year": {
+        "easy": {
+            "snapshot_start": "2020-01-01",
+            "snapshot_end": "2021-01-01",
+        },
+        "difficult": {
+            "snapshot_start": "1987-01-01",
+            "snapshot_end": "1988-01-01",
+        },
+    },
+    "electrolysis": {
+        "low": {"electrolysis_cost": 0.5},
+        "high": {"electrolysis_cost": 2},
+    },
+    "CCS": {
+        "low": {
+            "smr_cc_cost": 0.5,
+            "smr_cost": 0.5,
+            "co2_sequestration_potential": 2000,
+        },
+        "high": {
+            "smr_cc_cost": 2,
+            "smr_cost": 2,
+            "co2_sequestration_potential": 400,
+        },
+    },
+    "nuclear": {
+        "low": {
+            "nuclear_cost": 0.5,
+        },
+        "high": {
+            "nuclear_cost": 2,
+        },
+    },
+    "gas": {
+        "low": {
+            "gas_marginal_cost": 0.5,
+        },
+        "high": {
+            "gas_marginal_cost": 2,
+        },
+    },
+}
 
-combinations = [
-    dict(zip(config_values.keys(), values))
-    for values in itertools.product(*config_values.values())
+# Create set product of all scenario outcomes
+scenario_levels = {k: list(v.keys()) for k, v in scenarios.items()}
+scenario_levels = [[(k, v) for v in levels] for k, levels in scenario_levels.items()]
+combinations = list(itertools.product(*scenario_levels))
+
+
+def merge_dicts(list_of_dicts):
+    return {k: v for d in list_of_dicts for k, v in d.items()}
+
+
+config_combinations = [
+    merge_dicts([scenarios[s][l] for (s, l) in c]) for c in combinations
 ]
 
 with open(filename, "w") as f:
-    for i, config in enumerate(combinations):
+    for i, config in enumerate(config_combinations):
         f.write(template.format(scenario_number=i, **config))
