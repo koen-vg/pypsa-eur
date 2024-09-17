754dee96f6f61e47b8885a4161aa73c3effc726f
Koen van Greevenbroek <koen.v.greevenbroek@uit.no>
2024-09-17 12:18:44 +0200

    Add EU-hydrogen config

--- config/create_scenarios.py
+++ config/create_scenarios.py
@@ -13,25 +13,80 @@ else:
 
 import itertools
 
-# Insert your config values that should be altered in the template.
-# Change `config_section` and `config_section2` to the actual config sections.
-template = """
-scenario{scenario_number}:
-    config_section:
-        config_key: {config_value}
-    config_section2:
-        config_key2: {config_value2}
-"""
+filename_stripped = filename.split("/")[-1]
 
-# Define all possible combinations of config values.
-# This must define all config values that are used in the template.
-config_values = dict(config_value=["true", "false"], config_value2=[1, 2, 3, 4])
+if filename_stripped == "scenarios.yaml":
+    template = """
+    {scenario_name}:
+        snapshots:
+            start: "{year}-01-01 00:00"
+            end: "{year}-12-31 23:00"
+            inclusive: "both"
+        renewable:
+            onwind:
+                cutout: europe-era5_{year}
+            solar:
+                cutout: europe-era5_{year}
+            offwind-ac:
+                cutout: europe-era5_{year}
+            offwind-dc:
+                cutout: europe-era5_{year}
+            offwind-float:
+                cutout: europe-era5_{year}
+            hydro:
+                cutout: europe-era5_{year}
+        solar_thermal:
+            cutout: europe-era5_{year}
+        lines:
+            dynamic_line_rating:
+                cutout: europe-era5_{year}
+        sector:
+            heat_demand_cutout: europe-era5_{year}
+        biomass:
+            scenario: {biomass_scenario}
+    """
 
-combinations = [
-    dict(zip(config_values.keys(), values))
-    for values in itertools.product(*config_values.values())
+    scenarios = {
+        "Y": {
+            "1987": {
+                "year": "1987",
+            },
+            # "2013": {
+            #     "year": "2013",
+            # },
+            "2020": {
+                "year": "2020",
+            },
+        },
+        "B": {
+            "a": {
+                "biomass_scenario": "ENS_Low",
+            },
+            "b": {
+                "biomass_scenario": "ENS_Med",
+            },
+            "c": {
+                "biomass_scenario": "ENS_High",
+            },
+        },
+    }
+
+# Create set product of all scenario outcomes
+scenario_levels = {k: list(v.keys()) for k, v in scenarios.items()}
+scenario_levels = [[(k, v) for v in levels] for k, levels in scenario_levels.items()]
+combinations = list(itertools.product(*scenario_levels))
+
+
+def merge_dicts(list_of_dicts):
+    return {k: v for d in list_of_dicts for k, v in d.items()}
+
+
+config_combinations = [
+    merge_dicts([scenarios[s][l] for (s, l) in c]) for c in combinations
 ]
 
 with open(filename, "w") as f:
-    for i, config in enumerate(combinations):
-        f.write(template.format(scenario_number=i, **config))
+    for c, config in zip(combinations, config_combinations):
+        # Construct name string from c
+        name = "_".join([f"{s}{l}" for s, l in c])
+        f.write(template.format(scenario_name=name, **config))
